13110bee66de79b3975f435565a8abce35937dee
Michael F. Herbst <info@michael-herbst.com>
2021-01-31 19:26:33 +0100

    Update

--- 5.0.0/run.jl
+++ 5.0.0/run.jl
@@ -1,37 +1,33 @@
-using Test
 using DFTK
+using HDF5
+using Libxc
+using LinearAlgebra
 
-@testset "Spin-broken silicon setup relaxes to spin-paired ground state" begin
-    function run_silicon(spin_polarization)
-        lattice = [0.0  5.131570667152971 5.131570667152971;
-                   5.131570667152971 0.0 5.131570667152971;
-                   5.131570667152971 5.131570667152971  0.0]
-        Si = ElementPsp(:Si, psp=load_psp("hgh/lda/si-q4"))
-        model = model_PBE(lattice, [Si => [ones(3)/8, -ones(3)/8]],
-                          spin_polarization=spin_polarization, temperature=0.01)
+if !isfile("../sigma.hdf5")
+    lattice = diagm([3.0, 0.0, 0.0])
+    Si = ElementPsp(:Si, psp=load_psp("hgh/lda/si-q4"))
+    model = model_PBE(lattice, [Si => [ones(3)/8, -ones(3)/8]],
+                      spin_polarization=:collinear, temperature=0.01)
 
-        Ecut = 7
-        basis = PlaneWaveBasis(model, Ecut; kgrid=[2, 2, 2])
+    Ecut = 2
+    basis = PlaneWaveBasis(model, Ecut; kgrid=[1, 1, 1])
 
-        if spin_polarization == :collinear
-            ρspin = from_real(basis, 1.0rand(basis.fft_size...))
-        else
-            ρspin = nothing
-        end
-        self_consistent_field(basis, tol=5e-6, ρspin=ρspin, n_bands=10);
-    end
+    ρ = guess_density(basis).real
+    ρspin = rand(basis.fft_size...)
+    ρspin[abs.(ρspin) .> ρ] .= 0.0
+    ρspin = from_real(basis, ρspin)
+    ρ     = from_real(basis, ρ)
 
-    scfres        = run_silicon(:none)
-    scfres_broken = run_silicon(:collinear)
-    εbroken       = scfres_broken.eigenvalues
-
-    @test scfres.energies.total ≈ scfres_broken.energies.total atol=1e-5
-    absmax(x) = maximum(abs, x)
-    for (ik, kpt) in enumerate(scfres.basis.kpoints)
-        kequiv = findall(kbr -> kbr.coordinate == kpt.coordinate, scfres_broken.basis.kpoints)
-
-        for ikb in kequiv
-            @test scfres.eigenvalues[ik][1:10] ≈ εbroken[ikb][1:10] atol=5e-4 norm=absmax
-        end
-    end
+    density = DFTK.LibxcDensity(basis, 1, ρ, ρspin)
+    h5write("../sigma.hdf5", "ρ", density.ρ_real)
+    h5write("../sigma.hdf5", "σ", density.σ_real)
 end
+ρ_real = reshape(h5read("../sigma.hdf5", "ρ"), :)
+σ_real = reshape(h5read("../sigma.hdf5", "σ"), :)
+
+@show ρ_real
+@show σ_real
+xc = Functional(:gga_c_pbe; n_spin=2)
+zk = evaluate(xc; rho=ρ_real, sigma=σ_real).zk
+@show zk
+nothing
